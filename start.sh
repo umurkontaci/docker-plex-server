182565fd7c5d68a8b442956a480f814dc94fbb25
Umur Kontaci <fastreload@gmail.com>
2015-03-19 14:36:19 +0200

    use exec

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ rm "/root/Library/Application Support/Plex Media Server/plexmediaserver.pid"
 echo "Running Proxy"
 simpleproxy -L 0.0.0.0:50000 -R 127.0.0.1:32400 -d -a admin:admin
 echo "Running PMS"
-/usr/sbin/start_pms
+exec /usr/sbin/start_pms
